5d6a3c41438d49a777d55294f6b1fca966555aff
Gloria Kengne <theresagloria2004@gmail.com>
2025-12-03 11:41:50 +0100

    authentication implemented

---
+++ Med-Backend/backend/controllers/auth.controller.js
@@ -0,0 +1,57 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    // Check if user exists
+    const exists = await User.findOne({ email });
+    if (exists) return res.status(400).json({ message: "Email already used" });
+
+    // Hash password
+    const hashed = await bcrypt.hash(password, 10);
+
+    // Create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashed,
+      role
+    });
+
+    res.json({ message: "User registered", user });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Find user
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid email" });
+
+    // Compare password
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) return res.status(400).json({ message: "Wrong password" });
+
+    // Generate JWT
+    const token = jwt.sign(
+      { id: user._id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.json({
+      message: "Login successful",
+      token,
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
---
+++ Med-Backend/backend/routes/auth.routes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { register, login } from "../controllers/auth.controller.js";
+import authMiddleware from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+// Public routes
+router.post("/register", register);
+router.post("/login", login);
+
+// Protected route example
+router.get("/profile", authMiddleware, (req, res) => {
+  res.json({ message: "Your profile data", user: req.user });
+});
+
+export default router;
